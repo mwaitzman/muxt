0df34cb652b898ca63e851ac4a6dc65b1bee7e9e
mwaitzman <51432220+mwaitzman@users.noreply.github.com>
2021-10-22 02:15:16 -0400

    now creates AlternateScreen and prints IO device information

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
 use cpal::{
     traits::{DeviceTrait, HostTrait,},
     available_hosts, default_host,
 };
-fn main() -> Result<(), anyhow::Error> {
+use crossterm::{
+    terminal,
+    Result, execute, queue, style,
+};
+use std::io::{self, Write};
+fn main() -> Result<()> {
     println!("Starting the program...");
-    println!("Available hosts: {:?}", available_hosts());
+    let mut stdout = io::stdout();
+    run(&mut stdout)
+}
+fn run<W>(w: &mut W) -> crossterm::Result<()>
+where
+    W: Write,
+{
+    execute!(
+        w, 
+        terminal::EnterAlternateScreen,
+    )?;
+    terminal::enable_raw_mode()?;
+    execute!(
+        w,
+        style::Print(format!("{:?}\n", available_hosts()))
+    )?;
     let host = default_host();
-    print!("Using {:?} as the host", host.id());
-    println!(" with {} as the output device", host.default_output_device().unwrap().name().unwrap());
+    execute!(
+        w,
+        style::Print(format!("Using {:?} as the host", host.id())),
+        style::Print(format!(" with {} as the output device", host.default_output_device().unwrap().name().unwrap())),
+    )?;
     drop(host);
-    Ok(())
+    loop {
+    }
+    /*#[allow(unreachable_code)]
+    Ok(())*/
 }
